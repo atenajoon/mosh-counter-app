1f706d9a4fd897e2a5420ca185daba70bdbd132d
Atena Razm <atenarazm@gmail.com>
2021-02-11 15:11:55 -0800

    badge classes toggle dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 3
+        count: 0
     }
 
     render() {
+
         return (
             <React.Fragment>
-                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span className={this.toggleBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
 
         );
+    }
+
+    toggleBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
