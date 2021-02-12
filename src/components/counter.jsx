c6b86057fd812e91a241b621e19dbb78966d609d
Atena Razm <atenarazm@gmail.com>
2021-02-12 10:03:39 -0800

    handleIncrement() is updating state.count, array listing practice is commented out

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,15 +10,21 @@ class Counter extends Component {
 
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        // tags: ['tag1', 'tag2', 'tag3']
     }
 
     render() {
         return (
             <React.Fragment>
                 <span className={this.toggleBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-                {this.renderTags()}
+                {/* <button onClick={() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button> */}
+                <button
+                    onClick={this.handleIncrement}
+                    className="btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>
+                {/* {this.renderTags()} */}
             </React.Fragment>
 
         );
@@ -28,14 +34,14 @@ class Counter extends Component {
     //     console.log("this", this);
     // }
 
-
+    // handleIncrement = product => {
     handleIncrement = () => {
-        console.log("this", this);
+        this.setState({ count: this.state.count + 1 })
     }
 
-    renderTags() {
-        return this.state.tags.length === 0 ? <p>There are no tags!</p> : <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-    }
+    // renderTags() {
+    //     return this.state.tags.length === 0 ? <p>There are no tags!</p> : <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    // }
 
     toggleBadgeClasses() {
         let classes = "badge m-2 badge-";
